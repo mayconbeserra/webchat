1422e9149380d3e05f31b1ab49a8ef8ea3546c95
Maycon Beserra <maycon.beserra@auctionata.com>
2017-03-10 12:29:13 +0100

    chore: add the initial structure for knex

---
+++ server/knexfile.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const config = require('./config');
+
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: {
+      user: 'postgres',
+      password: 'sql',
+      database: 'webchat',
+      host: config.default.env.db.host,
+    },
+    migrations: {
+      directory: path.join(__dirname, '/db/migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, '/db/seeds/test'),
+    },
+  },
+  development: {
+    client: 'pg',
+    connection: {
+      user: 'postgres',
+      password: 'sql',
+      database: 'webchat',
+      host: config.default.env.db.host,
+    },
+    migrations: {
+      directory: path.join(__dirname, '/db/migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, '/db/seeds/development'),
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE,
+    migrations: {
+      directory: path.join(__dirname, '/db/migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, '/db/seeds/production'),
+    },
+  },
+};
