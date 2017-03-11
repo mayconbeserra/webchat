68790813eb5517a10c39431a5c14238ef40e98d7
Maycon Beserra <maycon.beserra@auctionata.com>
2017-03-11 15:01:35 +0100

    feat: add the socketio - basic structure

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import expressValidator from 'express-validator';
+import socketIO from 'socket.io';
+import http from 'http';
 import api from './api';
+
+const people = {};
+let clientUsers = [];
 
 export async function application () {
   const app = express();
 
   app.use(bodyParser.json());
   app.use(expressValidator());
+
   api(app);
 
   app.use((err, req, res, next) => { // eslint-disable-line consistent-return,no-unused-vars
@@ -27,13 +33,47 @@ const handleError = (err, res) => {
   return res.sendStatus(500);
 };
 
+const updatePeople = (username, id) => {
+  const userArray = [];
+  for (const key in people) {
+    if (people[key] === username) {
+      return;
+    }
+    userArray.push(people[key]);
+  }
+  userArray.push(username);
+  people[id] = username;
+  clientUsers = userArray;
+};
+
+const handleChat = (io) => {
+  io.on('connection', (socket) => {
+    console.log('Client Connected');
+    socket.on('join', (username) => {
+      updatePeople(username, socket.id);
+      io.emit('users-updated', clientUsers);
+    });
+    socket.on('disconnect', () => {
+      console.log('Client Disconnected');
+      delete people[socket.id];
+    });
+    socket.on('message', (message) => {
+      console.log(message);
+      io.emit('receive-message', message)
+    });
+  });
+};
+
 export async function start (config) {
   try {
     const app = await application(config);
-    console.log('## Debugging ### ');
-    app.listen(config.env.http.port, config.env.http.host, () => {
-      console.log(`listening on ${config.env.http.host}:${config.env.http.port}`); /* eslint no-console:0 */
-    });
+    const server = http.createServer(app);
+    const io = socketIO.listen(server);
+
+    server.listen(config.env.http.port);
+    console.log(`listening on ${config.env.http.host}:${config.env.http.port}`); /* eslint no-console:0 */
+
+    handleChat(io);
   } catch (err) {
     console.log(`A critical error happened: ${err}`);
   }
