43825a3864632d4c84daba725b65cf1801b564bc
Maycon Beserra <mayconbeserra@hotmail.com>
2017-03-13 23:41:25 +0100

    fix: correct the package json

--- server/src/app.js
+++ server/src/app.js
@@ -27,7 +27,8 @@ export async function start (config) {
     const io = socketIO.listen(server);
 
     server.listen(config.env.http.port);
-    console.log(`listening on ${config.env.http.host}:${config.env.http.port}`); /* eslint no-console:0 */
+    console.log(`listening backend on ${config.env.http.host}:${config.env.http.port}`); /* eslint no-console:0 */
+    console.log('listening frontend on http://0.0.0.0:8081');
 
     chatService(io);
   } catch (err) {
